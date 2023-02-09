22c3e0b27c9e0999764f37ac5122d251975849d4
A1 <asean@jenosize.com>
2023-02-09 18:48:41 +0700

    lucky

--- companemt/luckydraw-main.js
+++ companemt/luckydraw-main.js
@@ -1,11 +1,9 @@
 import { Button, Typography } from '@mui/material'
-import { Box, height, Stack } from '@mui/system'
+import { Box, Stack } from '@mui/system'
 import React, { useState } from 'react'
 import localFont from '@next/font/local'
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import Footernavbar from '../companemt/recover-footer'
-import { purple, red } from '@mui/material/colors';
-import Link from 'next/link';
 
 
 const myFont = localFont({ src: '../gamer_2/Gamer.ttf' })
@@ -27,24 +25,58 @@ const theme = createTheme({
     },
 });
 
+const Popup = ({index}) => {
+
+    return (
+        <Box sx={{ width: "200px", height: "50px", bgcolor: '#fafafa' }}>
+            <Typography>
+                xxxxxxx{index}
+            </Typography>
+        </Box>
+    );
+}
 
 
 function infectmain() {
+
+
+
+
     const [state, setstate] = useState('circle');
+    
+    const [index, setindex] = useState(0);
 
 
+    //                 0         1       2       3       4       5      6       7        8       9      10     11
+
+    const fruits = [4100.33, 4260.33, 4427.33, 4593.33, 4760.33, 4930.33, 5100.33, 5270.33, 5440.33, 5610.33, 5770.33, 5930.33]
+    
+
+
+    let number = fruits[index]
+    
 
 
     const startRotation = () => {
+        
+        setindex (Math.floor(Math.random() * fruits.length+1))
         setstate("circle start-rotate");
         console.log(state)
         setTimeout(() => {
             setstate("circle start-rotate stop-rotate")
 
-        }, Math.floor(Math.random() * 8000) + 1000);
+        }, Math.floor(Math.random() * 150 + number));
+        // setTimeout(() => {
+        //     setstate("circle start-rotate stop-rotate")
 
+        // },5930.33);
+        console.log(index)
     }
+
+
+
     return (
+
         <Box width={"100%"} sx={{ pt: "20px" }}>
             <Stack alignItems={"center"}>
 
@@ -115,16 +147,28 @@ function infectmain() {
 
 
                 <Button onClick={startRotation} variant="contained" color='error'
-                sx={{ height: "54px", width: '186px', mt: 0, boxShadow: "inset 0 -0.9em 0 -0.35em rgba(0,0,0,0.17)" ,zIndex:2}}>
+                    sx={{ height: "54px", width: '186px', mt: 0, boxShadow: "inset 0 -0.9em 0 -0.35em rgba(0,0,0,0.17)", zIndex: 2 }}>
                     <Typography className={myFont.className} sx={{ p: 0, fontSize: "30px", height: "50px" }} >
                         SPIN
                     </Typography>
                 </Button>
+                {index == 1 ? <Popup index={index}/>  : ""}
+                {index == 2 ? <Popup index={index}/>  : ""}
+                {index == 3 ? <Popup index={index}/> : ""}
+                {index == 4 ? <Popup index={index}/> : ""}
+                {index == 5 ? <Popup index={index}/> : ""}
+                {index == 6 ? <Popup index={index}/> : ""}
+                {index == 7 ? <Popup index={index}/> : ""}
+                {index == 9 ? <Popup index={index}/> : ""}
+                {index == 10 ? <Popup index={index}/> : ""}
+                {index == 11 ? <Popup index={index}/> : ""}
+                {index == 12 ? <Popup index={index}/> : ""}
+
 
 
 
             </Stack>
-            <Footernavbar/>
+            <Footernavbar />
 
 
         </Box>
