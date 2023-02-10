f3428c825750a2e57271a3f79d73a2d1fb9770b8
A1 <asean@jenosize.com>
2023-02-10 14:24:43 +0700

    spinnig

--- companemt/luckydraw-main.js
+++ companemt/luckydraw-main.js
@@ -1,6 +1,6 @@
 import { Button, Typography } from '@mui/material'
 import { Box, Stack } from '@mui/system'
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import localFont from '@next/font/local'
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import Footernavbar from '../companemt/recover-footer'
@@ -11,28 +11,37 @@ const myFont = localFont({ src: '../gamer_2/Gamer.ttf' })
 
 
 
-const theme = createTheme({
-    palette: {
-        primary: {
-            // Purple and green play nicely together.
-            main: "#0ACD7A",
-        },
-        palette: {
-            // This is green.A700 as hex.
-            main: '',
-        },
 
-    },
-});
-
-const Popup = ({index}) => {
+const Popup = ({ gift, reset }) => {
 
     return (
-        <Box sx={{ width: "200px", height: "50px", bgcolor: '#fafafa' }}>
-            <Typography>
-                xxxxxxx{index}
-            </Typography>
+        <Box sx={{ width: "100%", height: "100%", display: 'flex', alignItems: 'center', justifyContent: 'center', position: 'fixed', zIndex: 1000, pb:{ xs:'195px',sm:"360px",md:"700px"} }}>
+            <Box sx={{ width: "100%", height: "100%", bgcolor: '#000', position: 'absolute', opacity: 0.7 }}>
+
+            </Box>
+
+            <Box className={myFont.className}  sx={{pt:20,zIndex: 1100,color:'#fff',fontSize:{xs:50,sm:80} }}>
+
+                {/* // YOU GIFT IS  */}
+
+                YOU GIFT IS <Typography color={'#F7EF8A'} display={"flex"} alignItems={"center" } justifyContent={"center"} className={myFont.className} sx={{fontSize:{xs:70,sm:100}}} >{gift}</Typography>
+
+                {/* /// botton reset */}
+                <Box sx={{ width: "100%", height: "100%", display: 'flex', alignItems: 'center', justifyContent: "center",mt:5 }}>
+                    <Button onClick={reset} variant="contained" color='error'
+                        sx={{ display: 'flex', alignItems: 'center', justifyContent: "center", height: "54px", width: '186px', boxShadow: "inset 0 -0.9em 0 -0.35em rgba(0,0,0,0.17)", zIndex: 2 }}>
+                        <Typography className={myFont.className} sx={{ p: 0, fontSize: "30px", height: "50px" }} >
+                            COMFIRM
+                        </Typography>
+                    </Button>
+                </Box>
+
+            </Box>
+
+
+
         </Box>
+
     );
 }
 
@@ -40,44 +49,64 @@ const Popup = ({index}) => {
 function infectmain() {
 
 
+    const reset = () => {
 
+        setstate('circle')
+        setIdxPop()  
+        
+    }
 
     const [state, setstate] = useState('circle');
-    
-    const [index, setindex] = useState(0);
+    const [IdxPop, setIdxPop] = useState();
 
 
-    //                 0         1       2       3       4       5      6       7        8       9      10     11
+    let time = [4100.33, 4260.33, 4427.33, 4593.33, 4760.33, 4930.33, 5100.33, 5270.33, 5440.33, 5610.33, 5770.33, 5930.33]
+    let jacpot = ["600$", "500$", "400$", "300$", "200$", "100$", "50$", "LOSE", "900$", "800$", "700$", "JACKPOT"]
 
-    const fruits = [4100.33, 4260.33, 4427.33, 4593.33, 4760.33, 4930.33, 5100.33, 5270.33, 5440.33, 5610.33, 5770.33, 5930.33]
-    
+    const [index, setindex] = useState();
+
+    useEffect(() => {
+        setindex(Math.floor(Math.random() * time.length))
+    }, [])
+    const [gift, setGift] = useState();
 
 
-    let number = fruits[index]
-    
 
+    let number = time[index]
+
+    console.log("index top = ", index)
+    console.log("gift top = ", gift)
 
     const startRotation = () => {
         
-        setindex (Math.floor(Math.random() * fruits.length+1))
+        setindex(Math.floor(Math.random() * time.length))
         setstate("circle start-rotate");
-        console.log(state)
         setTimeout(() => {
             setstate("circle start-rotate stop-rotate")
+            setIdxPop(index)
+            setGift(jacpot[index])
 
         }, Math.floor(Math.random() * 150 + number));
+
+        console.log("number in = ", number)
+        console.log("index in = ", index)
+        console.log("setindex in = ", index)
+        // setTimeout(() => {
+        //     setNumber(number = fruits[index])
+
+        // }, Math.floor(Math.random() * 150 + number));
         // setTimeout(() => {
         //     setstate("circle start-rotate stop-rotate")
 
         // },5930.33);
-        console.log(index)
+        // console.log(setTimeout)
     }
 
 
 
     return (
 
-        <Box width={"100%"} sx={{ pt: "20px" }}>
+        <Box width={"100%"} sx={{ pt:{xs:"20px",sm:"100px",md:"230px"} }}>
             <Stack alignItems={"center"}>
 
                 {/* Font */}
@@ -152,17 +181,18 @@ function infectmain() {
                         SPIN
                     </Typography>
                 </Button>
-                {index == 1 ? <Popup index={index}/>  : ""}
-                {index == 2 ? <Popup index={index}/>  : ""}
-                {index == 3 ? <Popup index={index}/> : ""}
-                {index == 4 ? <Popup index={index}/> : ""}
-                {index == 5 ? <Popup index={index}/> : ""}
-                {index == 6 ? <Popup index={index}/> : ""}
-                {index == 7 ? <Popup index={index}/> : ""}
-                {index == 9 ? <Popup index={index}/> : ""}
-                {index == 10 ? <Popup index={index}/> : ""}
-                {index == 11 ? <Popup index={index}/> : ""}
-                {index == 12 ? <Popup index={index}/> : ""}
+                {IdxPop == 0 ? <Popup gift={gift} reset={reset}/> : ""}
+                {IdxPop == 1 ? <Popup gift={gift} reset={reset}/> : ""}
+                {IdxPop == 2 ? <Popup gift={gift} reset={reset}/> : ""}
+                {IdxPop == 3 ? <Popup gift={gift} reset={reset}/> : ""}
+                {IdxPop == 4 ? <Popup gift={gift} reset={reset}/> : ""}
+                {IdxPop == 5 ? <Popup gift={gift} reset={reset}/> : ""}
+                {IdxPop == 6 ? <Popup gift={gift} reset={reset}/> : ""}
+                {IdxPop == 7 ? <Popup gift={gift} reset={reset}/> : ""}
+                {IdxPop == 8 ? <Popup gift={gift} reset={reset}/> : ""}
+                {IdxPop == 9 ? <Popup gift={gift} reset={reset}/> : ""}
+                {IdxPop == 10 ? <Popup gift={gift} reset={reset}/> : ""}
+                {IdxPop == 11 ? <Popup gift={gift} reset={reset}/> : ""}
 
 
 
